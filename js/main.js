8162ed64bd31cd04feaabb8b593248e2a1ac1a03
Alex <alexkhotenoff@gmail.com>
2020-04-24 01:40:27 +0300

    add map scroll lock for mobile devices

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 var advantages = document.querySelector(".advantages");
 
-
 const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
     e.preventDefault()
-    
+
     const blockID = anchor.getAttribute('href').substr(1)
-    
+
     document.getElementById(blockID).scrollIntoView({
       behavior: 'smooth',
       block: 'start'
     })
   })
 }
-
 
 //advantages tabs
 if (advantages) {
@@ -52,52 +50,69 @@ function advantages_init(advantages_block) {
 
 //slider
 
-$(document).ready(function(){
-    $('.bxslider').bxSlider({
-        adaptiveHeight: true,
-        controls: true,
-        pager: true,
-        video: true,
-        minSlides: 1,
-        infiniteLoop: true,
-    });
+$(document).ready(function () {
+  $('.bxslider').bxSlider({
+    adaptiveHeight: true,
+    controls: true,
+    pager: true,
+    video: true,
+    minSlides: 1,
+    infiniteLoop: true,
   });
+});
 
-//Yandex.maps
+//yandex.maps
+var map_container = document.querySelector('.contacts__map-container');
+var map_overlay = document.querySelector('.contacts__map-overlay');
+var overlay_button = document.querySelector('.contacts__map-overlay-button');
 
-  try {
-    //yandex.maps API
-    ymaps.ready(function () {
-      var myMap = new ymaps.Map('map', {
-        center: [64.554778, 40.539630],
-        zoom: 17,
-        controls: ['zoomControl', 'routeButtonControl']
-      }),
-  
-        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-          '<div style="color: #35475f; background-color: #ffffff; border-radius: 5px;' +
-          'padding:5px; font-weight: bold; width: 200px; text-align: center; opacity: 90%">$[properties.iconContent]</div>'
-        ),
-  
-        myPlacemarkWithContent = new ymaps.Placemark([64.554778, 40.539630], {
-          iconContent: 'Пункт отбора на военную службу по контракту',
-          hintContent: 'Пункт отбора на военную службу по контракту',
-        }, {
-          iconLayout: 'default#imageWithContent',
-          iconImageHref: 'img/map_marker_new.png',
-          iconImageSize: [57, 60],
-          iconImageOffset: [-11, -70],
-          iconContentOffset: [-90, -50],
-          iconContentLayout: MyIconContentLayout
-        });
-  
-        myMap.behaviors.disable('scrollZoom');
-        // myMap.behaviors.disable('drag'); 
-        // myMap.behaviors.disable('multiTouch');
-      myMap.geoObjects
-        .add(myPlacemarkWithContent);
-    });
+overlay_button.addEventListener('click', function () {
+  if (map_overlay.classList.contains('contacts__map-overlay_active')) {
+    map_overlay.classList.remove('contacts__map-overlay_active');
   }
-  catch {
-    console.log("Yandex maps is not defined at this page");
-  }
+});
+
+try {
+  //yandex.maps API
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+      center: [64.554778, 40.539630],
+      zoom: 17,
+      controls: ['zoomControl', 'routeButtonControl']
+    }),
+
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #35475f; background-color: #ffffff; border-radius: 5px;' +
+        'padding:5px; font-weight: bold; width: 200px; text-align: center; opacity: 90%">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemarkWithContent = new ymaps.Placemark([64.554778, 40.539630], {
+        iconContent: 'Пункт отбора на военную службу по контракту',
+        hintContent: 'Пункт отбора на военную службу по контракту',
+      }, {
+        iconLayout: 'default#imageWithContent',
+        iconImageHref: 'img/map_marker_new.png',
+        iconImageSize: [57, 60],
+        iconImageOffset: [-11, -70],
+        iconContentOffset: [-90, -50],
+        iconContentLayout: MyIconContentLayout
+      });
+
+    // //на мобильных устройствах... (проверяем по userAgent браузера)
+    // if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    //   //... отключаем перетаскивание карты
+    //   myMap.behaviors.disable('drag');
+    // }
+
+    //disable mouse scroll
+    myMap.behaviors.disable('scrollZoom');
+    // myMap.behaviors.disable('drag'); 
+    // myMap.behaviors.disable('multiTouch');
+
+    myMap.geoObjects
+      .add(myPlacemarkWithContent);
+  });
+}
+catch {
+  console.log("Yandex maps is not defined at this page");
+}
